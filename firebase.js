3e1cba59bfdceef2ddccd98357be3fa91e0e379e
Rabbil khan <rabbilkhan125@gmail.com>
2023-09-16 15:40:01 +0530

    adding signup firebase

---
+++ firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuHwlQ6ztLeFqUJm6zv-XHogMy4BsqBBM",
+  authDomain: "react-login-auth-e3533.firebaseapp.com",
+  projectId: "react-login-auth-e3533",
+  storageBucket: "react-login-auth-e3533.appspot.com",
+  messagingSenderId: "562493439398",
+  appId: "1:562493439398:web:65e44d0e21966dadfea367"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
