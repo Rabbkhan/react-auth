2f51fb95ed3e54075560916b6efc1804b5ec7854
Rabbil khan <rabbilkhan125@gmail.com>
2023-10-01 21:05:08 +0530

    login coding part

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -6,49 +6,66 @@ const AuthForm = () => {
   const passwordInputRef = useRef('');
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setLoading] = useState(false);
-
+// const [dataflash, setDataflash] = useState(false)
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
-
+  const resetForm = () => {
+    emailInputRef.current.value = '';
+    passwordInputRef.current.value = '';
+  };
   const submitHandler = (event) => {
     event.preventDefault();
 
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
+setLoading(true);
+// setDataflash(true)
+let url;
 
     if (isLogin) {
       // Handle login
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDuHwlQ6ztLeFqUJm6zv-XHogMy4BsqBBM'
+      
     } else {
       setLoading(true);
+url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDuHwlQ6ztLeFqUJm6zv-XHogMy4BsqBBM'
+}
+fetch(url, {
+   method: 'POST',
+   body: JSON.stringify({
+     email: enteredEmail,
+     password: enteredPassword,
+     returnSecureToken: true
+   }),
+   headers: {
+     'Content-Type': 'application/json'
+   }
+ })
+   .then((res) => {
+     setLoading(false);
+     resetForm()
 
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDuHwlQ6ztLeFqUJm6zv-XHogMy4BsqBBM', {
-        method: 'POST',
-        body: JSON.stringify({
-          email: enteredEmail,
-          password: enteredPassword,
-          returnSecureToken: true
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-        .then((res) => {
-          if (res.ok) {
-            // Sign-up successful, you can handle it as needed
-          } else {
-            return res.json().then((data) => {
-              console.log(data.error.message); // Log the error message
-            });
-          }
-        })
-        .catch((error) => {
-          console.error('Error:', error); // Log any other errors
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-    }
+     if (res.ok) {
+      return res.json()
+       // Sign-up successful, you can handle it as needed
+      //  alert('Signup succesfully')
+     } else {
+       return res.json().then((data) => {
+         console.log(data.error.message); // Log the error message
+         let errorMessage  = 'Authentication failed';
+         throw new Error (errorMessage)
+       });
+     }
+   })
+   .then((data)=>{
+console.log(data)
+
+   })
+   .catch((error) => {
+     alert(error.message)
+   })
+   
   };
 
   return (
@@ -70,10 +87,9 @@ const AuthForm = () => {
         </div>
         <div className={classes.actions}>
           <button
-            type='submit'
-            disabled={isLoading}
-          >
-            {isLogin ? 'Login' : 'Create Account'}
+            type='submit'>
+           {!isLoading?(isLogin ? 'Login' : 'Create Account')
+           :( <p>Loading...</p>)}
           </button>
           <button
             type='button'
